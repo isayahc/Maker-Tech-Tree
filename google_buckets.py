28957101328144d627e69261c8c965e4a5e73f7b
isayhc <isayahculbertson@gmail.com>
2024-03-07 01:11:21 -0500

    storing 3d models

--- google_buckets.py
+++ google_buckets.py
@@ -1,11 +1,31 @@
 from google.cloud import storage
+import os
+import json
 
+SERVICE_ACOUNT_STUFF = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+
+# https://stackoverflow.com/questions/71878229/initializing-firebase-admin-via-environment-variables-without-storing-serviceacc
+key_dict = json.loads(
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"]
+)
+
+SERVICE_ACOUNT_STUFF = os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON')
+
+
+# fire_app = firebase_admin.initialize_app(Certificate(key_dict))
+
+
+# db = client = firestore.client(app=fire_app)
 # Initialize Google Cloud Storage client
 client = storage.Client()
 
 # Define bucket and file name
-bucket_name = 'your-bucket-name'
-file_name = 'your-file.glb'
+# bucket_name = os.getenv('GOOGLE_PROJECT_ID')
+bucket_name = "production-blender-platform-bucket"
+# file_name = 'your-file.glb'
+file_name = "tmpcpd7o7v0.glb"
+
+
 
 # Function to upload a .glb file to the bucket
 def upload_file(file_path):
@@ -30,7 +50,8 @@ def delete_file():
 
 def main():
     # Example usage
-    upload_file('path/to/your/file.glb')
+    file_name = "tmpcpd7o7v0.glb"
+    upload_file(file_name)
     download_file('path/to/save/your/file.glb')
     delete_file()
 
